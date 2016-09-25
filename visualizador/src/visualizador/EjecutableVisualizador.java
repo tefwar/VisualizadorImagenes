2327b3c73f59c5f59a75b84fd147225a92aa8424
DANIEL <DANIEL@DESKTOP-E1MUHF6>
2016-09-25 14:52:47 -0500

    Define clase Ejecutable y Logica y añade imagenes

--- visualizador/src/visualizador/EjecutableVisualizador.java
+++ visualizador/src/visualizador/EjecutableVisualizador.java
@@ -2,17 +2,18 @@ package visualizador;
 
 import processing.core.*;
 
-public class EjecutableVisualizador extends PApplet{
+public class EjecutableVisualizador extends PApplet {
 	Logica app;
 
-	public void setup(){
-	size(1200, 700);
-	background(0);
+	public void setup() {
+		size(1200, 700);
+		background(0);
+		app = new Logica(this);
 	}
-	
-	public void draw(){
-		
-		//app.pintar();
-		
+
+	public void draw() {
+
+		app.pintar();
+
 	}
 }
